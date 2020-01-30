2c509dcc7182780331ac1eb987ad3d59f68490a2
Lucas Humphries <zhac140@cim-ts-node-03.cc.rhul.local>
2020-01-30 12:22:42 +0000

    Beginning of Creature class

--- Creatures.py
+++ Creatures.py
@@ -1,3 +1,14 @@
 class Creature:
-    def __init__(self, pos):
+    def __init__(self, pos, radius):
         self.pos = pos #centre of sprite
+        self.radius = radius
+        self.speed = 0
+
+
+
+
+class Player(Creature):
+    def __init__(self, pos):
+        playerRadius = 20
+        super().__init__(pos, playerRadius)
+        

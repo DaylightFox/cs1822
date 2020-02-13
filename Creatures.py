ba8539d3ac3495ca1f36c103f20c2e9a9f9bd5bf
Lucas Humphries <zhac140@cim-ts-node-01.cc.rhul.local>
2020-02-13 14:51:28 +0000

    Added sprite field  take_damage

--- Creatures.py
+++ Creatures.py
@@ -1,18 +1,35 @@
 class Creature:
-    def __init__(self, pos, radius):
+    def __init__(self, pos, radius, sprite):
         self.pos = pos #centre of sprite
         self.radius = radius
+        self.sprite = sprite
         self.speed = 0
         self.level = 1
         self.maxHpBase = 1
         self.maxHpMultiplier = 1
         self.currentHp = 1
+        self.DmgBase = 1
 
-
-
+    def take_damage(self, damage):
+        self.currentHp -= damage
+        
 
 class Player(Creature):
     def __init__(self, pos):
         playerRadius = 20
-        super().__init__(pos, playerRadius)
-        self.baseDmg = 1
+        sprite = 1#replace with default sprite
+        super().__init__(pos, playerRadius, sprite)
+        self.speed = 1
+
+
+
+
+class Enemy(Creature):
+    def __init__(self, )
+
+    def take_damage(self, damage):
+        self.currentHp -= damage
+        #self.killed()
+
+    #def killed(self):
+        

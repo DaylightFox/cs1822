0714f72c22f390f3155a02dfb2a6190cc0604953
Lucas Humphries <zhac140@cim-ts-node-01.cc.rhul.local>
2020-02-14 11:45:12 +0000

    Added file with basic imports

---
+++ TestCreatures.py
@@ -0,0 +1,8 @@
+try:
+    import simplegui
+except ImportError :
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+try:
+    from Vector import Vector
+except ImportError:
+    from user304_rsf8mD0BOQ_1 import Vector

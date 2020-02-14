a5bb0270659617b196cb9c01494151b825ca6e1c
Lucas Humphries <zhac140@cim-ts-node-01.cc.rhul.local>
2020-02-14 12:28:14 +0000

    Added border to represent room

--- TestCreatures.py
+++ TestCreatures.py
@@ -6,3 +6,27 @@ try:
     from Vector import Vector
 except ImportError:
     from user304_rsf8mD0BOQ_1 import Vector
+
+
+#from map_gen import Room
+from Creatures import *
+
+WIDTH, HEIGHT = 400, 400
+
+def draw(canvas):
+    canvas.draw_polygon([(0,0),(WIDTH,0),(WIDTH,HEIGHT),(0,HEIGHT)], 5, "red")
+    #room.draw(canvas)
+
+
+#room = Room(350, 350, (WIDTH/2, HEIGHT/2))
+
+
+
+
+
+
+
+frame = simplegui.create_frame("Test Creatures", WIDTH, HEIGHT)
+frame.set_draw_handler(draw)
+
+frame.start()

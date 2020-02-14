de8ecc61e29e1b531e14deb1811c712d2a8301e5
Lucas Humphries <zhac140@cim-ts-node-01.cc.rhul.local>
2020-02-14 16:39:39 +0000

    Added draw method and related attributes

--- Creatures.py
+++ Creatures.py
@@ -8,12 +8,18 @@ class Creature:
         self.pos = pos #centre of sprite
         self.radius = radius
         self.sprite = sprite
+        self.center_source = []
+        self.width_height_source = [] 
+        self.width_height_dest = []
         self.speed = 0
         self.level = 1
         self.maxHpBase = 1
         self.maxHpMultiplier = 1
         self.currentHp = 1
         self.DmgBase = 1
+        
+    def draw(self, canvas):
+        canvas.draw_image(self.sprite, self.center_source, self.width_height_source, self.pos, self.width_height_dest)
 
     def take_damage(self, damage):
         self.currentHp -= damage

5612e2551b16de4a9c03590710881a3db3df4480
Lucas Humphries <zhac140@cim-ts-node-01.cc.rhul.local>
2020-02-18 15:05:44 +0000

    Added simplegui import

--- Creatures.py
+++ Creatures.py
@@ -2,6 +2,10 @@ try:
     from Vector import Vector
 except ImportError:
     from user304_rsf8mD0BOQ_1 import Vector
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 class Creature:
     def __init__(self, pos, radius, sprite):

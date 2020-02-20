48ba5d2f6c9bf0dcf86fb19974b2e474aa687f85
Thazin Oo <zhac339@live.rhul.ac.uk>
2020-02-20 14:20:11 +0000

    Projectiles bounces off sides

--- FinalCorridor.py
+++ FinalCorridor.py
@@ -3,6 +3,7 @@ try:
 except ImportError :
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
+
 from Vector import*
 from animation import*
 
@@ -10,7 +11,9 @@ import random
 
 
 WIDTH = 300
-HEIGHT = 700
+HEIGHT = 500
+
+numProjectiles = 4
 
 def randCol ():
     r = random.randrange (0, 256)
@@ -18,15 +21,30 @@ def randCol ():
     b = random.randrange (0, 256)
     return 'rgb('+str(r)+ ','+str(g)+ ','+str(b)+ ')'
 
+def ranPos ():
+    x = random.randint(10,WIDTH)
+    y = -10
+    return Vector(x,y)
+
 class Projectiles:
-    def __init__(self, pos, colour): #initialized all projectiles to be the same
+    def __init__(self, pos): # all projectiles are the same except for where on the CANVAS WIDTH they spawn
         self.pos = pos
         self.radius = 10
         self.vel = Vector(0, 2)
-        self.colour = colour
+        self.colour = 'red'
         self.border = 1
+
+    def bounce(self, normal):
+        self.vel.reflect(normal)
+
+    def walls(self):
+        if(self.pos.x <= 0):
+            self.bounce( Vector(1, 0) )
+        elif(self.pos.x >= WIDTH):
+            self.bounce( Vector(1, 0))
     
     def update(self):
+        self.walls()
         self.pos.add(self.vel)
 
     def draw(self,canvas):
@@ -36,31 +54,42 @@ class Projectiles:
                            self.colour,
                            self.colour)
 
-
-ProjectilesList = []
-
-def makeProjectiles():
-    global ProjectilesList
-    fire = Projectiles()
-
-    for i in range(1,6):
-        n = random.randint(10,290)
-        colour = randCol()
-        fire = Projectiles(n, colour)
-        ProjectilesList.append(fire)
+class Interaction:
+    def __init__(self, sprite, projectiles):
+        self.sprite = sprite
+        self.projectiles = projectiles
+        self.numProjectiles = numProjectiles #constant
 
 
-def draw(canvas):
-    makeProjectiles()
+    def draw(self, canvas):
+        global i
+        
+        if i < len(self.projectiles):
+            self.projectiles[i].draw(canvas)
+            self.projectiles[i].update()
 
-    
+            if self.projectiles[i].pos.y >= HEIGHT + 30:
+                print(self.projectiles)
+                i+= 1
+                #self.projectiles.remove(self.projectiles[i])
+                #print("Prev Ball: " + str(i - 1))
+                #print("Current ball: " + str(i))
+                #print("List Length: " + str (len(self.projectiles)))
+                
+        
+        #else:
+            #when i is more than number of projectiles
+            #maybe call end game sequence?
 
-    #inter.update()
-    #bunny.update()
-    #bunny.draw(canvas)
+i=0
+ProjectilesList = [Projectiles(ranPos()) for i in range(numProjectiles) ]
+spriteCollide = Interaction(bunny,ProjectilesList) #sprite and projectile interaction definition
+
+
+
 
 frame = simplegui.create_frame("Collisions Corridor", WIDTH, HEIGHT)
-frame.set_draw_handler(draw)
+frame.set_draw_handler(spriteCollide.draw)
 frame.set_keydown_handler(kbd.keyDown)
 frame.set_keyup_handler(kbd.keyUp)
 frame.start()

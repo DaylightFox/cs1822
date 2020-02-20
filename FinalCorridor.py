94b707357f1f045a00ab81c76b236d51e1ac2689
Thazin Oo <zhac339@live.rhul.ac.uk>
2020-02-20 15:11:34 +0000

    Animation added. Diagonal Projectiles initialized

--- FinalCorridor.py
+++ FinalCorridor.py
@@ -5,33 +5,46 @@ except ImportError :
 
 
 from Vector import*
-from animation import*
+from animation import MC
+from animation import Keyboard
+from animation import Interaction
 
 import random
 
 
-WIDTH = 300
+WIDTH = 200
 HEIGHT = 500
+numProjectiles = 5
 
-numProjectiles = 4
+mc_img = simplegui.load_image("https://i.imgur.com/hpehVFb.png")
+mc_width = 128
+mc_height = 128
+mc_columns = 4
+mc_rows = 4
+
+def ranPos ():
+    x = random.randint(10,WIDTH)
+    y = -10
+    return Vector(x,y)
+'''
+def randVel():
+    x = random.randint(-5, 5)
+    y = random.randint(-5, 5)
+    return( Vector(x, y) )
 
 def randCol ():
     r = random.randrange (0, 256)
     g = random.randrange (0, 256)
     b = random.randrange (0, 256)
     return 'rgb('+str(r)+ ','+str(g)+ ','+str(b)+ ')'
-
-def ranPos ():
-    x = random.randint(10,WIDTH)
-    y = -10
-    return Vector(x,y)
+'''
 
 class Projectiles:
-    def __init__(self, pos): # all projectiles are the same except for where on the CANVAS WIDTH they spawn
+    def __init__(self, pos, vel, colour): # all projectiles are the same except for where on the CANVAS WIDTH they spawn
         self.pos = pos
         self.radius = 10
-        self.vel = Vector(0, 2)
-        self.colour = 'red'
+        self.vel = vel
+        self.colour = colour
         self.border = 1
 
     def bounce(self, normal):
@@ -54,36 +67,46 @@ class Projectiles:
                            self.colour,
                            self.colour)
 
-class Interaction:
-    def __init__(self, sprite, projectiles):
-        self.sprite = sprite
-        self.projectiles = projectiles
+class SpriteProjectileInteraction:
+    def __init__(self, sprite, projectVertical, projectDiagonal):
+        self.bunny = sprite
+        self.projectVertical = projectVertical
+        self.projectDiagonal = projectDiagonal
         self.numProjectiles = numProjectiles #constant
 
 
     def draw(self, canvas):
-        global i
+        global i, bunnyMove
         
-        if i < len(self.projectiles):
-            self.projectiles[i].draw(canvas)
-            self.projectiles[i].update()
+        if i < len(self.projectVertical):
+            self.projectVertical[i].draw(canvas)
+            self.projectVertical[i].update()
 
-            if self.projectiles[i].pos.y >= HEIGHT + 30:
-                print(self.projectiles)
+            if self.projectVertical[i].pos.y >= HEIGHT + 30:
+                print(self.projectVertical)
                 i+= 1
                 #self.projectiles.remove(self.projectiles[i])
                 #print("Prev Ball: " + str(i - 1))
                 #print("Current ball: " + str(i))
                 #print("List Length: " + str (len(self.projectiles)))
-                
+
+        bunnyMove.update()
+        self.bunny.update()
+        self.bunny.draw(canvas)        
         
         #else:
             #when i is more than number of projectiles
             #maybe call end game sequence?
 
-i=0
-ProjectilesList = [Projectiles(ranPos()) for i in range(numProjectiles) ]
-spriteCollide = Interaction(bunny,ProjectilesList) #sprite and projectile interaction definition
+i= j = 0
+VProjectilesList = [Projectiles(ranPos(), Vector(0,5), "red" ) for i in range(numProjectiles) ]
+DProjectilesList = [Projectiles(ranPos(), Vector(3,5), "blue") for j in range(numProjectiles)]
+
+bunny = MC(Vector(WIDTH/2, HEIGHT - 100), mc_img, mc_width, mc_height, mc_columns, mc_rows)
+kbd = Keyboard()
+bunnyMove = Interaction(bunny, kbd)
+
+spriteCollide = SpriteProjectileInteraction(bunny,VProjectilesList, DProjectilesList) #sprite, vertical and diagonal projectiles interaction definition
 
 
 
@@ -92,4 +115,5 @@ frame = simplegui.create_frame("Collisions Corridor", WIDTH, HEIGHT)
 frame.set_draw_handler(spriteCollide.draw)
 frame.set_keydown_handler(kbd.keyDown)
 frame.set_keyup_handler(kbd.keyUp)
+
 frame.start()

6af2ad4067e1189b8b51e56dde369a53c5fc03ad
Lucas Humphries <zhac140@cim-ts-node-02.cc.rhul.local>
2020-02-20 14:33:28 +0000

    Converted mouse_pos into Vector

--- Creatures.py
+++ Creatures.py
@@ -53,6 +53,7 @@ class Wizard(Player):
         """
         Vector: mouse_pos 
         """
+        mouse_pos = Vector(mouse_pos[0], mouse_pos[1])
         direction = (mouse_pos - self.pos).normalise
         distance = 1#replace with value (probably level scale)
         attack = ConeAttack(self.pos, direction, distance)

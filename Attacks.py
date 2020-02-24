d8e1331ea736da016806bc7237bbc0f99f79b6c8
Lucas Humphries <zhac140@cim-ts-node-01.cc.rhul.local>
2020-02-24 19:32:53 +0000

    Added ProjectileAttack class

--- Attacks.py
+++ Attacks.py
@@ -4,7 +4,7 @@ except ImportError:
     from user304_rsf8mD0BOQ_1 import Vector
 
 class Attack:
-    def __init__(self, damage, pos):
+    def __init__(self, pos, damage):
         self.damage = damage
         self.pos = pos
         self.sprite = 1  #replace with place holder
@@ -19,6 +19,21 @@ class ConeAttack(Attack):
         self.direction = direction
         self.distance = distance
         self.angle = angle
+        #self.launch()
+    
+    def hit_creature(self, creature):
+        difference = creature.pos - self.pos
+        if difference.length <= self.distance:
+            angle = self.direction.angle(difference.get_normalized())
+            if angle <= self.angle:
+                return True
+        return False
+
+class ProjectileAttack(Attack):
+    def __init__(self, pos, direction, radius):
+        super().__init__(damage, pos)
+        self.direction = direction
+        self.radius = radius
         #self.launch()
     
     def hit_creature(self, creature):

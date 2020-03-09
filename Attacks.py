d501a2dbb9ab5e341810c020f808753da0a2845c
Lucas Humphries <zhac140@cim-ts-node-02.cc.rhul.local>
2020-03-09 15:36:36 +0000

    fixed typo

--- Attacks.py
+++ Attacks.py
@@ -30,8 +30,6 @@ class ConeAttack(Attack):
             angle = self.direction.angle(difference.get_normalized())
             if angle <= self.angle:
                 return True
-            
-            edge = self.direction.copy().rotate(self.angle) * difference.length()
             else:
                 edge = self.direction.copy().rotate(self.angle) * difference.length()
                 if (edge - creature.pos).length() <= creature.radius:

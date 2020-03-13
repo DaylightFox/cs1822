107f28f7a9aef412f9d88c149b308d08b151acab
Thazin Oo <zhac339@live.rhul.ac.uk>
2020-03-13 13:52:22 +0000

    Fixed images

--- startScreen.py
+++ startScreen.py
@@ -4,8 +4,8 @@ try:
 except ImportError :
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
-from animationV3 import MC
 from Vector import Vector
+from animationV4 import*
 
 '''
 when going to a different stage
@@ -20,39 +20,53 @@ HEIGHT = 500
 
 class Start():
     def __init__(self):
+        self.spaceKey = False
+        self.title = ["Welcome to...", "Trip: Down The Rabbit Hole"]
+        self.storyText = ["HUH... WHERE AM I?", "It's another one...", "... get him...", "!!! I NEED TO GET OUT OF HERE"]
         self.wasd = simplegui._load_local_image("Images/WASDkeys.png")
         self.wasdPos = (60,150)
-        self.left = simplegui._load_local_image("Images/leftClick.png")
+        self.left = simplegui._load_local_image("Images/leftKey.png")
         self.leftPos = (50, 250)
-        self.right = simplegui._load_local_image("Images/rightClick.png")
+        self.right = simplegui._load_local_image("Images/rightKey.png")
         self.rightPos = (50, 350)
-        self.mc = MC(Vector(100,300))
+        self.mc = MC(Vector(WIDTH/2,HEIGHT/2))
         self.size = (2048, 2048)
         self.resize = (150,150)
         self.centre = (2048/2, 2048/2)
 
+    def goToNext(self):
+        return (self.key == simplegui.KEY_MAP['space'])
+
     
     def tutorial(self, canvas):
+        i=0
         canvas.draw_text('Tutorial', (180, 50), 30, 'white', 'monospace')
         canvas.draw_text('Move Character', (150, 150), 20, 'white', 'monospace')
         canvas.draw_text('Left Click & Drag to Shoot', (150, 250), 20, 'white', 'monospace')
         canvas.draw_text('Right Click to ____', (150, 350), 20, 'white', 'monospace')
+
+        #timerText(self.title, self.title.length())
         
         canvas.draw_image(self.wasd, 
                          self.centre,
                          self.size,
                          self.wasdPos,
                          self.resize)
+    
         canvas.draw_image(self.left, 
                          self.centre,
                          self.size,
                          self.leftPos,
                          self.resize)
+
         canvas.draw_image(self.right, 
                          self.centre,
                          self.size,
                          self.rightPos,
                          self.resize)
+    
+        
+            
 
     
     #def keyDown(self):
@@ -63,5 +77,6 @@ class Start():
 
 #frame = simplegui.create_frame('Start Screen', WIDTH, HEIGHT)
 #frame.set_draw_handler(startScreen.tutorial)
-#frame.set_keydown_handler(startScreen.keyDown)
+#timer = simplegui.create_timer(800, timer_handler)
+#timer.start()
 #frame.start()

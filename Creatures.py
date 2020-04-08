436f8698893f5e51bfa48109eaf951747238de01
Lucas Humphries <zhac140@cim-ts-node-01.cc.rhul.local>
2020-04-08 22:13:53 +0100

    set default direction vector

--- Creatures.py
+++ Creatures.py
@@ -18,7 +18,7 @@ class Creature:
         self.width_height_source = []
         self.width_height_dest = [self.radius*2, self.radius*2]
         self.speed = 0
-        self.direction = 1#unit vector
+        self.direction = Vector(0,0)#unit vector
         self.attackList = []
         self.level = 1
         self.levelScaleMultplier = 1
